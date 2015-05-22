a51f58da1cc298f25e38e0ed1ee14197474c85c1
siberiawolf <yuxiangliang0307@gmail.com>
2015-05-22 16:08:59 +0800

    通过原型继承创建一个新对象

--- examples/06.01.inherit.js
+++ examples/06.01.inherit.js
@@ -1,13 +1,13 @@
-// inherit() returns a newly created object that inherits properties from the
-// prototype object p.  It uses the ECMAScript 5 function Object.create() if
-// it is defined, and otherwise falls back to an older technique.
-function inherit(p) {
-    if (p == null) throw TypeError(); // p must be a non-null object
-    if (Object.create)                // If Object.create() is defined...
-        return Object.create(p);      //    then just use it.
-    var t = typeof p;                 // Otherwise do some more type checking
+// 通过原型继承创建一个新对象
+function inherit(p) {	// p必须是一个对象
+    if (p == null) throw TypeError();  
+    if (Object.create)                // Object.create是ECMAScript 5中新加的函数
+        return Object.create(p);      // 存在的话，就直接使用   
+    var t = typeof p;                 // 在ECMAScript 3中用prototype继承
     if (t !== "object" && t !== "function") throw TypeError();
-    function f() {};                  // Define a dummy constructor function.
-    f.prototype = p;                  // Set its prototype property to p.
-    return new f();                   // Use f() to create an "heir" of p.
+    function f() {};                   
+    f.prototype = p;                  // 将空函数的原型指定为p对象
+    return new f();                   // 返回空构造函数
 }
+// 最早提出原型继承的人：
+// http://javascript.crockford.com/prototypal.html
